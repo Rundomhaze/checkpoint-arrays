c5c6e22895c0f8a43551c7198cbf65f97e0a7e17
Taras <posh@live.ru>
2021-04-07 09:51:38 +0300

    added empty functions (for tests)

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
+const filterByAge = () => {
+  // your code
+};
+
+const filterByScore = () => {
+  // your code
+};
+
+const filterByServiceRecord = () => {
+  // your code
+};
+
+const sortByScore = () => {
+  // your code
+};
+
+const getMeanAge = () => {
+  // your code
+};
+
+const getMeanAgeReduce = () => {
+  // your code
+};
+
+const getNamesOnly = () => {
+  // your code
+};
 
 module.exports = {
   filterByAge,
